1627b81931d8463b5d9d5f9592b987e59b5cae26
rayssadavvila <rayssamunizd@gmail.com>
2022-09-19 10:46:58 -0300

    Criação dos construtores nas classes

--- Cliente.java
+++ Cliente.java
@@ -2,5 +2,10 @@ public class Cliente {
     private String nomeUser;
     private String email;
     private int senha;
-    
+
+    public Cliente(String nomeUser, String email, int senha){
+        this.nomeUser=nomeUser;
+        this.email=email;
+        this.senha=senha;
+    }    
 }
--- Cozinheiros.java
+++ Cozinheiros.java
@@ -3,6 +3,16 @@ public class Cozinheiros {
     private String email;
     private int senha;
     private String[] cardapio;
-    private float avaliacao;
-    
+    private int quantidadeDeCardapios;
+    private float avaliacao; 
+    private float mediaAvaliacao;//media das avaliacoes
+    public Cozinheiros(String nomeUser, String email, int senha, String cardapio, int quantidadeDeCardapios, float avaliacao, float mediaAvaliacao){
+        this.nomeUser=nomeUser;
+        this.email=email;
+        this.senha=senha;
+        this.cardapio= new String [quantidadeDeCardapios];
+        this.quantidadeDeCardapios=quantidadeDeCardapios;
+        this.avaliacao=avaliacao;
+        this.mediaAvaliacao=mediaAvaliacao;
+    }
 }
