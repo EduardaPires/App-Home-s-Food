0741983e807f2090cf9b3fc5236e93702d2c5d25
EduardaPires <pireseduarda526@gmail.com>
2022-09-19 10:37:41 -0300

    processo de encapsulamento

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 //1. encapsulamento, herança/classe abstrata, exception, solid, adicionar 3 padrões de projeto
 public class Main {
 
-
-
     public static void main(String[] args) {
-    
+    //perguntar se o cliente quer fazer pedido, se sim mostrar cardapio, se for cozinheiro mostra pedidos recebidos ou deixar adicionar pratos
     }
+
+    
 
 
 }
--- Pedidos.java
+++
@@ -1,9 +0,0 @@
-public class Pedidos {
-    private int quantPedidos; 
-    private String[] pedidos; //o tamanho será a quantia de pedidos
-
-    public void MostrarPedidos() {
-        //irá mostrar ao cliente todos os cozinheiros disponiveis
-    }
-
-}
