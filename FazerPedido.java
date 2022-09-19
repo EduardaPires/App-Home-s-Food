fe13b7c3d2934214279eee52880f7f4884fb3f81
EduardaPires <pireseduarda526@gmail.com>
2022-09-19 10:37:12 -0300

    Início da implementação do projeto Home's Food

---
+++ FazerPedido.java
@@ -0,0 +1,15 @@
+public class FazerPedido {
+    private int quantPedidos; 
+    private String[] pedidos; //o tamanho será a quantia de pedidos
+    private int avaliacao; //fazer calculo de média, só vai adiconar, não aparece a avaliação individual; e será feita após o pedido (perguntando se foi recebido)
+
+    public void mostrarPedidos() {
+
+    }
+    
+    public String[] selecionarPedidos() {
+        //mostraPedidos()
+        return null;
+    }
+
+}
