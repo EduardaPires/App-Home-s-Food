458afac1c2ac0dda81b5fd80733e2c74fd6f2d09
rayssadavvila <rayssamunizd@gmail.com>
2022-09-26 11:05:08 -0300

    Continuação da construção da classe pai "Pessoa"

--- Pessoa.java
+++ Pessoa.java
@@ -2,14 +2,34 @@ public class Pessoa {
     //atributos que cliente e cozinheiros tem em comum
     String nome;
     String email;
-    String senha;
+    int senha;
 
     //construtor(inicializar as variaveis de instancia)
-    public Pessoa(String nome, String email, String senha){
+    public Pessoa(String nome, String email, int senha){
         this.nome=nome;
         this.email=email;
         this.senha=senha;
     }
-    
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public int senha(){
+        return this.senha;
+    }
+    public void setSenha(int senha){
+        this.senha=senha;
+    }
 
 }
