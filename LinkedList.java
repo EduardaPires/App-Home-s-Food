d75b748381abfc19d8b3afd898a9cd8e4e67d18f
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-10-01 23:29:26 -0300

    isEmpty( ) e insertEnd( ) da classe LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -10,7 +10,21 @@ public class LinkedList<T> {
         }
     }
 
-    
+    public void insertEnd(T pessoa){
+        Node valor = new Node(pessoa);
+
+        if(this.isEmpty()){
+            this.first = valor;
+        }
+        else{
+            Node aux = this.first;
+            while(aux.getNext() != null){
+                aux = aux.getNext();
+            }
+            aux.setValue(valor);
+        }
+
+    }
 
 
     
