f525ea9f3060c933029db32424ff188b99652ca3
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-10-01 23:10:28 -0300

    Classe LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,17 @@
+public class LinkedList<T> {
+    private Node<T> first;
+
+    public boolean isEmpty(){
+        if(this.first == null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    
+
+
+    
+}
