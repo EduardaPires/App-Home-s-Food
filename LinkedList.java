a381f0823caf8794234140dbf613b78c08486f70
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-10-03 08:51:19 -0300

    criando método se inserção ordenada(com erro)

--- LinkedList.java
+++ LinkedList.java
@@ -12,6 +12,42 @@ public class LinkedList<T> {
         }
     }
 
+    public void voidInserirOrdenado(T valor) {
+		Node novo = new Node(valor);
+		Node anterior = null, atual;
+		
+		if(this.isEmpty()) {
+			this.primeiro = novo;
+			this.ultimo = novo;
+			this.qtd ++;
+		}
+		else if(valor.compareTo(this.primeiro.getValue()) < 0){ // inserção no início
+			 novo.setProx(this.primeiro);
+			 this.primeiro = novo;
+			 this.qtd ++;
+		}
+		else if(valor.compareTo(this.ultimo.getValue()) > 0) { // inserção no final
+			this.ultimo.setNext(novo);
+			this.ultimo = novo;
+			this.qtd ++;
+		}
+		else { // inserção no "meio" da lista
+			atual = this.primeiro;
+			while(atual != null) {
+				if(atual.getValue().compareTo(valor) > 0) { //achou ponto de inserção
+					anterior.setNext(novo);
+					novo.setNext(atual);
+					qtd++;
+					break;
+				}
+				else {
+					anterior = atual;
+					atual = atual.getNext();
+				}
+			}
+		}
+	}
+
     //testando commit no notebook para ver se aparece
 
 
